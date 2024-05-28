4373b05e5750ec58b33e37c776f3a88082d1ea41
DavidEliauri <eliauri.david@gmail.com>
2024-05-28 22:36:48 +0300

    added fonts & implemented prettier-tailwind plugin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,4 +2,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
+    theme: {
+        fontFamily: {
+            Inter: ['Inter', 'sans-serif'],
+            Montserrat: ['Montserrat', 'sans-serif'],
+        },
+        extend: {
+            colors: {
+                'tenchat-black': '#09090B',
+            },
+        },
+    },
 };
